53dba6736519f5fbffce32d9409aea73f09ad5e0
Daniil Savva <savvadaniil25@gmail.com>
2019-09-11 10:56:09 +0300

    Initial Commit

---
+++ MIOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MIOC
+//
+//  Created by Daniil Savva on 11/09/2019.
+//  Copyright © 2019 Daniil Savva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
